6d1639d33c75d4372133b033750564e1950c167f
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2022-08-16 14:13:51 +0530

    -Execution time of writing the dataframe in parquet,csv format while using coalesce and repartition.

--- repartition_coalesce.py
+++ repartition_coalesce.py
@@ -8,23 +8,23 @@ if __name__ == '__main__':
         '/Users/maheshvakkund/PycharmProjects/pythonProject/python-pyspark/data-master/sales.csv',
         header=True, inferSchema=True)
     st = time.time()
-    csv_df.repartition(4).write. \
-        mode("Overwrite"). \
-        option("compression", "none"). \
-        format('parquet'). \
+    csv_df. \
+        coalesce(4). \
+        write. \
+        mode("overwrite"). \
+        format("csv"). \
         save("/Users/maheshvakkund/PycharmProjects/pythonProject/python-pyspark/data-master/writepath")
     et = time.time()
     elapsed_time = et - st
     print('Execution time:', elapsed_time, 'seconds')
 
     st = time.time()
-    csv_df.coalesce(4).write. \
-        mode("Overwrite"). \
-        option("compression", "none"). \
-        format('parquet'). \
+    csv_df. \
+        repartition(5). \
+        write. \
+        mode("overwrite"). \
+        format("csv"). \
         save("/Users/maheshvakkund/PycharmProjects/pythonProject/python-pyspark/data-master/writepath")
     et = time.time()
     elapsed_time = et - st
     print('Execution time:', elapsed_time, 'seconds')
-
-
